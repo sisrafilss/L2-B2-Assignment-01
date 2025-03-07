211f9deb59039ec98a84498d240fcd57dbecdbac
Israfil Hossen <sisrafilss@gmail.com>
2025-03-07 09:09:48 +0600

    solution 1 addded

--- task.ts
+++ task.ts
@@ -1,1 +1,8 @@
-console.log("hello");
+// soution 1
+type numberOrStringType = number | string;
+function tsTestFunction(param: numberOrStringType): numberOrStringType {
+  if (typeof param === "string") return param.length;
+  else return param * param;
+}
+
+// soution 2
